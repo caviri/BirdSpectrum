e7679be5178478d25b6a3e76f589611d98bedbe5
Carlos Vivar Rios <carlos.vivarrios@unil.ch>
2021-04-28 14:22:37 +0200

    fixed low pass filter bug when loading

--- sketch.js
+++ sketch.js
@@ -23,17 +23,17 @@ function setup() {
 
   createCanvas(1200, 800); //1600,1600
   background(0);
-  
+
   pixelDensity(1);
-  
+
   //mic = new p5.AudioIn();
   //mic.start();
   fft = new p5.FFT();
   //fft.setInput(mic);
-  
+
   input = createFileInput(handleFile);
   input.position(width-300, 0);
-  
+
     filter = new p5.LowPass();
 
     // Disconnect soundfile from master output.
@@ -44,7 +44,7 @@ function setup() {
 }
 
 function draw() {
-  
+
   filterFreq = map(mouseX, 0, width, 10, 22050);
 
   // Map mouseY to resonance (volume boost) at the cutoff frequency
@@ -52,53 +52,53 @@ function draw() {
 
   // set filter parameters
   filter.set(filterFreq, filterRes);
-  
-  
+
+
   let spectrum = fft.analyze();
-  
-  
+
+
 
   loadPixels();
-  
+
    //var y = floor((pix/4)/width);
    //var x = floor(pix-(y*4));
-  
+
 
   //console.log(width);
   //console.log(x);
   //console.log(pixels.length);
-  
+
   //for ( var p =0; p < speed; p +=4){
-    
+
 //     r = random(r, r+5);
 //     g = random(g, g+5);
 //     b = random(b, b+5);
-    
+
 //     if (r > 255){r = 0};
 //     if (g > 255){g = 0};
 //     if (b > 255){b = 0};
-    
+
     //line(0, y, 85, 75);
 
     for ( var x =  0; x <width; x++){
-      
+
       var pixel = (x+y*width)*4;//(x*4)+((width*4)*y);
-      
+
       var index_s = int(map(x, 0, width, 0, spectrum.length));
-      
+
       var color = spectrum[index_s]; //map(spectrum[index_s], 0, 255, 255, 0)
-      
+
       pixels[pixel] = color;
       pixels[pixel+1] = 0;
       pixels[pixel+2] = 0;
       pixels[pixel+3] = 200;
-      
+
     }
 
-      
+
     //}
 //     //for (var le = 0; le < Lextension; le++){
-    
+
 //         pixels[pix+p] = r;
 //         pixels[pix+p+1] = g;
 //         pixels[pix+p+2] = b;
@@ -108,14 +108,14 @@ function draw() {
     //color(r,g,b); //map(spectrum[p], 0, 255, 255, 0);
 
   if (y > height){y=0;}else{y += 1;};
-      
+
   updatePixels();
-  
+
   fill(0);
   stroke(0);
 
   rect(width-200, 100,180,100);
-  
+
   let waveform = fft.waveform();
   noFill();
   beginShape();
@@ -126,16 +126,16 @@ function draw() {
     vertex((width-200)+x, 100+yy);
   }
   endShape();
-  
+
   //if (pix > pixels.length){pix=0};
-  
+
 }
 
 // function mousePressed() {
 //   if (song.isPlaying()) {
 //     // .isPlaying() returns a boolean
 //     song.stop();
-    
+
 //   } else {
 //     song.play();
 
@@ -147,12 +147,12 @@ function keyPressed() {
     if (song.isPlaying()) {
     // .isPlaying() returns a boolean
     song.stop();
-    
+
   } else {
     song.play();
 
   }
-    
+
   }
 }
 
@@ -163,9 +163,14 @@ function handleFile(file) {
     // .isPlaying() returns a boolean
     song.stop();
     song = loadSound(file);
+    song.disconnect();
+    song.connect(filter);
     song.play();
   } else {
     song = loadSound(file);
+
+    song.disconnect();
+    song.connect(filter);
     song.play();
 
   }
